087ccdd925dd4191129ffd038d1f267286ed6565
panl <panlei106@gmail.com>
2014-03-18 20:23:16 +0800

    add PetsActivity and MarketActivity

---
+++ src/org/linxiangyu/mmbaby/activity/PetsActivity.java
@@ -0,0 +1,18 @@
+package org.linxiangyu.mmbaby.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import org.linxiangyu.mmbaby.R;
+
+/**
+ * Created by Inner on 14-3-18.
+ */
+public class PetsActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pets);
+
+    }
+}

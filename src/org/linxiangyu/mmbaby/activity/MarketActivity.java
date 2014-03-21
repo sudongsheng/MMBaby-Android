20f53a35b9bc0e8208ecdd0af9942cb13e0e2761
sudongsheng <sudongsheng123@gmail.com>
2014-03-21 11:18:02 +0800

    add some pictures

--- src/org/linxiangyu/mmbaby/activity/MarketActivity.java
+++ src/org/linxiangyu/mmbaby/activity/MarketActivity.java
@@ -13,6 +13,5 @@ public class MarketActivity extends Activity {
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_market);
-
     }
 }

02264497627b07e6cf0a04edcaf0440229483431
sudongsheng <sudongsheng123@gmail.com>
2014-04-16 13:36:45 +0800

    change the rate of three field of setting Activity

--- src/org/mmclub/mmbaby/utils/AppConstant.java
+++ src/org/mmclub/mmbaby/utils/AppConstant.java
@@ -5,6 +5,10 @@ public interface AppConstant {
     public final static int PET_CAT=2;
     public final static int PET_RABBIT=3;
 
+    public final static int MORALITY=1;
+    public final static int INTELLIGENCE=2;
+    public final static int PHYSICAL=3;
+
     public final static int Mama=0;
     public final static int Baby=1;
 }
